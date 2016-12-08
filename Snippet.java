00915eeaa46ae52b9e43d4f58d3c73dd7127abc9
Igor Rohal <igor.rohal@gmail.com>
2016-12-08 17:36:57 +0100

    Create Snippet.java

---
+++ Snippet.java
@@ -0,0 +1,8 @@
+@Test(expected = PrimaryAddressDeletionException.class)
+public void testPrimaryAddressDeletion()
+{
+    int primaryAddress = customer.getPrimaryAddress().getId();
+    addressService.deleteAddress(customer.getId(), primaryAddress.getId());
+    // interaction checks
+    Mockito.verify(dbService, never())).deleteDocument(customer.getPrimaryAddress().getId());
+}
